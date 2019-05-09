08628ba77abac78a5038748a45c5ed85637a3d28
Nikolay Penev (s5064371) <s5064371@bournemouth.ac.uk>
2019-05-09 16:05:37 +0100

    Added UI elements, buggy timer

---
+++ KatamariChronicles/Assets/Scripts/MassTracker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class MassTracker : MonoBehaviour {
+
+    public GameObject player;
+
+    void Update()
+    {
+        var sizeTracker = player.GetComponent<PlayerController>().size;
+        this.GetComponent<Text>().text = "Mass: " + Math.Round(sizeTracker,2) + "/10m";
+        
+    }
+}
---
+++ KatamariChronicles/Assets/Scripts/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour {
+
+    public float gameTimer=300;
+
+    bool gameStart;
+
+    bool gameEnd;
+
+    int seconds;
+    int minutes;
+    int hour;
+
+    void Start()
+    {
+        gameStart = true;
+    }
+    void Update()
+    {
+        
+        
+        while (gameStart == true)
+        {
+            gameTimer -= Time.deltaTime;
+            seconds = (int)(gameTimer % 60);
+            minutes = (int)(gameTimer / 60) % 60;
+            hour = (int)(gameTimer / 3600) / 24;
+
+            if (gameTimer <= 0)
+            {
+                gameStart = false;
+                gameEnd = true;
+            }
+        }
+        string timerString = string.Format("{0:0}:{1:00}:{2:00}", hour, minutes, seconds);
+        print(gameTimer);
+        this.GetComponent<Text>().text =timerString;
+    }
+}
