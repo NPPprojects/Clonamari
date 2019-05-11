dbab1a277544d6e79aa58ae276e38046b8bd22f0
NPPprojects <39345956+NPPprojects@users.noreply.github.com>
2019-05-11 16:46:35 +0100

    Added to HUD, began setting up mobile controls

--- KatamariChronicles/Assets/Scripts/HUD/PreviewItemName.cs
+++ KatamariChronicles/Assets/Scripts/HUD/PreviewItemName.cs
@@ -10,17 +10,31 @@ public class PreviewItemName : MonoBehaviour {
     int LastItem;
     bool DeleteName;
 
-    void Start()
-    {
-        
-    }
+    
 
     void Update()
     {
-
-
         PreviewNewestItem();
+        DeleteCheck();
+    }
 
+    void PreviewNewestItem()
+    {
+        print("Barely");
+        if (previewItem.GetComponent<PreviewItem>().previewState == true)
+        {
+            print("Works");
+            LastItem = player.GetComponent<PlayerController>().CollectedGameObjectList.Count - 1;
+            objectName = player.GetComponent<PlayerController>().CollectedGameObjectList[LastItem].gameObject.name;
+            this.GetComponent<Text>().text = objectName;
+            timer = 2;
+            DeleteName = true;
+
+        }
+        
+    }
+    void DeleteCheck()
+    {
         if (DeleteName == true)
         {
             timer -= Time.deltaTime;
@@ -30,20 +44,5 @@ public class PreviewItemName : MonoBehaviour {
                 DeleteName = false;
             }
         }
-    }
-
-    void PreviewNewestItem()
-    {
-        
-        if (previewItem.GetComponent<PreviewItem>().previewState == true)
-        {
-            LastItem = player.GetComponent<PlayerController>().CollectedGameObjectList.Count - 1;
-            objectName = player.GetComponent<PlayerController>().CollectedGameObjectList[LastItem].gameObject.name;
-            this.GetComponent<Text>().text = objectName;
-            timer = 2;
-            DeleteName = true;
-
-        }
-        
     }
 }
