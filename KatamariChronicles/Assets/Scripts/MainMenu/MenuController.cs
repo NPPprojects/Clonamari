669324c19815df7ec9b47aa7ee3faef3fd16f96d
Nikolay Penev (s5064371) <s5064371@bournemouth.ac.uk>
2019-05-12 21:50:19 +0100

    Menu added

---
+++ KatamariChronicles/Assets/Scripts/MainMenu/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+  
+  
+    public AudioSource musicSource;
+     public void PlayLevel1()
+     {
+        SceneManager.LoadScene("TestingLevel");
+     }
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene("Familiar");
+    }
+
+    public void QuitGame()
+     {
+        Application.Quit();
+     }
+
+    public void PlayMusic()
+    {
+        if (musicSource.isPlaying)
+            musicSource.Pause();
+        else
+            musicSource.Play();
+    }
+}
